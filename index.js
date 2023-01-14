60d300fd42e720e8bf76b126d5379f9408cb27e0
Cesar Colunga <cesara.colunga@gmail.com>
2023-01-13 22:23:05 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+// for loop assignment
+function writeCards(cards, eventName){
+    const cardLetters =[];
+ 
+    for (let i=0;i<cards.length;i++){
+        cardLetters[i] =`Thank you, ${cards[i]}, for the wonderful ${eventName} gift!`;
+        console.log(`Thank you, ${cards[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return cardLetters;
+}
+
+// while loop assignment
+function countDown(number){
+    let i = 0;
+    while (i<number+1){
+        console.log(number);
+        number -= 1;
+    }
+}
